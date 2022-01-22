8985089592f056132995bfb175d1b88bceae80cc
Masiel Villalba <villalbamasiel@gmail.com>
2022-01-22 14:09:26 -0500

    styling... bottons in corner

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -11,22 +11,35 @@ export class CalendarComponent implements OnInit {
   year = 2022;
   month = 1;
 
-  public yearMonth = new Date(this.year,this.month);  
+  public yearMonth = new Date(this.year,this.month);
 
   ngOnInit(): void {
-  }
-
-  public daysInMonth() : number {
-    return new Date(this.year, this.month, 0).getDate();
   }
 
   range(i: number) {
     return new Array(i);
   }
 
+  // Mal: febrero lo esta dando con 31 dias
+  public daysInMonth() : number {
+    return new Date(this.year, this.month, 0).getDate();
+  }
+
   public numberOfWeeks() : number{
     let inMonth = this.daysInMonth();
     return inMonth%7>0 ? Math.floor(inMonth/7) + 1 : Math.floor(inMonth/7); 
+  }
+
+  public prevMonth() {
+    this.month = this.month - 1 == 0 ? 12 : this.month-1;
+    this.year = this.month == 12 ? this.year - 1 : this.year; 
+    this.yearMonth = new Date(this.year,this.month);
+  }
+
+  public nextMonth() {
+    this.month = this.month + 1 == 13 ? 1 : this.month+1;
+    this.year = this.month == 1 ? this.year + 1 : this.year; 
+    this.yearMonth = new Date(this.year,this.month);
   }
 }
 

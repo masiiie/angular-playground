60ecb676cb485f638dd58123e17d77c80ff2894a
Masiel Villalba <villalbamasiel@gmail.com>
2022-01-23 14:41:17 -0500

    calendar inside library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CalendarComponent } from './calendar/calendar.component'
 
 const routes: Routes = [
-  { path: '', component:  CalendarComponent},
 ];
 
 @NgModule({

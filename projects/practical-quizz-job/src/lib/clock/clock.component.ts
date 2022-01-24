557cb0dace734657f39e198b535a1e588da4bc16
Masiel Villalba <villalbamasiel@gmail.com>
2022-01-23 23:48:20 -0500

    crono going forward...

--- projects/practical-quizz-job/src/lib/clock/clock.component.ts
+++ projects/practical-quizz-job/src/lib/clock/clock.component.ts
@@ -13,11 +13,18 @@ export class ClockComponent implements OnInit, OnDestroy {
   time = new Date();
   intervalId;
 
+  show = 'clock';
+  cronoHours = 0;
+  cronoMinutes = 0;
+  cronoSeconds = 0;
+  cronoInterval;
+
   constructor() {
     // Ejecuta el metodo en el intervalo dado
     this.intervalId = setInterval(() => {
       this.time = new Date();
     }, 1000);
+    this.cronoInterval = 0;
   }
 
   ngOnInit() {
@@ -25,5 +32,31 @@ export class ClockComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     clearInterval(this.intervalId);
+  }
+
+  startCrono(){
+    this.cronoInterval = setInterval(()=>{
+      this.cronoSeconds+=1;
+      if(this.cronoSeconds == 60){
+        this.cronoSeconds = 0;
+        this.cronoMinutes+=1;
+
+        if(this.cronoMinutes==60){
+          this.cronoMinutes=0;
+          this.cronoHours+=1;
+        }
+      }
+    });
+  }
+
+  pauseCrono(){
+    clearInterval(this.cronoInterval);
+  }
+
+  stopCrono() {
+    this.pauseCrono();
+    this.cronoHours = 0;
+    this.cronoMinutes = 0;
+    this.cronoSeconds = 0;
   }
 }

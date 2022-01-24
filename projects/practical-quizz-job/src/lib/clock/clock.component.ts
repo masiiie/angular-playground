df82b21f7c09d053f49f4d08ab032c51d5fde357
Masiel Villalba <villalbamasiel@gmail.com>
2022-01-24 01:19:12 -0500

    countDown almost ok

--- projects/practical-quizz-job/src/lib/clock/clock.component.ts
+++ projects/practical-quizz-job/src/lib/clock/clock.component.ts
@@ -21,6 +21,12 @@ export class ClockComponent implements OnInit, OnDestroy {
   cronoInterval:any;
   cronoRunning = false;
 
+  countdownHours = 0;
+  countdownMinutes = 0;
+  countdownSeconds = 5;
+  countDownInterval:any;
+  countDownRunning = false;
+
   constructor() {
     // Ejecuta el metodo en el intervalo dado
     this.intervalId = setInterval(() => {
@@ -32,7 +38,49 @@ export class ClockComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
+    this.restartAll();
+  }
+
+  changeMode(mode:string){
+    this.show = mode;
+  }
+
+  restartAll(){
+    this.stopCrono();
     clearInterval(this.intervalId);
+  }
+
+  startCountdown(){
+    if(this.countDownRunning){
+      this.countDownInterval = setInterval(()=>{
+        this.countdownSeconds-=1;
+        if(this.countdownSeconds<0){
+          this.countdownSeconds = 60;
+          this.countdownMinutes-=1;
+          if(this.countdownMinutes<0){
+            this.countdownMinutes=60;
+            this.countdownHours-=1;
+            if(this.countdownHours<0) this.stopCountdown();
+          }
+        }
+      }, 1000);
+    }
+    this.countDownRunning = true;
+  }
+
+  setCountdown(hours:number, minutes:number, seconds:number){
+    this.countdownHours=hours;
+    this.countdownMinutes=minutes;
+    this.countdownSeconds=seconds;
+  }
+
+  stopCountdown(){
+    this.countdownHours = 0;
+    this.countdownMinutes = 0;
+    this.countdownSeconds = 0;
+
+    clearInterval(this.countDownInterval);
+    this.countDownRunning = false;
   }
 
   startCrono(){

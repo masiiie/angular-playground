a20bc127d9fcc02523255393eb893d838550a47b
Masiel Villalba <villalbamasiel@gmail.com>
2022-01-24 05:01:44 -0500

    finished!

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'journalism-front-end'`, () => {
+  it(`should have as title 'practical-quizz'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('journalism-front-end');
+    expect(app.title).toEqual('practical-quizz');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('journalism-front-end app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('practical-quizz app is running!');
   });
 });

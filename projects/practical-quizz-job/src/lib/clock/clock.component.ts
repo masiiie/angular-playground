5f21a6dc587b1c5e504696392650f3edc0b4f986
Masiel Villalba <villalbamasiel@gmail.com>
2022-01-23 23:05:25 -0500

    clock ok!

--- projects/practical-quizz-job/src/lib/clock/clock.component.ts
+++ projects/practical-quizz-job/src/lib/clock/clock.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Subscription, TimeoutError, timer } from 'rxjs';
+import { map, share } from "rxjs/operators";
+import { OnDestroy } from '@angular/core';
+
 @Component({
   selector: 'lib-clock',
   templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.css']
 })
-export class ClockComponent implements OnInit {
+export class ClockComponent implements OnInit, OnDestroy {
+  time = new Date();
+  intervalId;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    // Ejecuta el metodo en el intervalo dado
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
   }
 
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
 }
